52bf9d12cdfb14a50cec08384090c78ae68fe347
Garon Fok <fokgaron@gmail.com>
2022-08-25 15:39:53 -0700

    Swap order of contact and resume buttons in CTA

--- src/components/CTA/index.tsx
+++ src/components/CTA/index.tsx
@@ -23,19 +23,19 @@ const CTA = () => {
             <div className="grid-cols-2 gap-4 py-2 mt-5 text-xl sm:grid">
               <button
                 type="button"
+                onClick={downloadResume}
+                className="w-full py-2 my-2 border rounded-lg border-slate-300 bg-slate-100 text-cyan-900 hover:bg-gray-200"
+              >
+                Download Resume
+              </button>
+              <button
+                type="button"
                 onClick={() => {
                   window.location.href = "mailto:fokgaron@gmail.com";
                 }}
                 className="w-full py-2 my-2 text-white rounded-lg shadow-md bg-cyan-700 hover:bg-cyan-900"
               >
                 Contact Me
-              </button>
-              <button
-                type="button"
-                onClick={downloadResume}
-                className="w-full py-2 my-2 border rounded-lg border-slate-300 bg-slate-100 text-cyan-900 hover:bg-gray-200"
-              >
-                Download Resume
               </button>
             </div>
           </div>
